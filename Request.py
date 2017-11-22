114170058a040bd83c0622bb68d95c73c2489bc6
Ron U <sharpblade4@gmail.com>
2017-11-22 07:38:30 +0200

    Update and rename request.py to Request.py

--- Request.py
+++ Request.py
@@ -1,6 +1,7 @@
 import os
 
-class request:
+# TODO documentation (@Ofir [look at Response and do something equivalent]) 
+class Request:
     def __init__(self, imgPath):
         if(os.path.exists(imgPath)):
             self._imgPath = imgPath
