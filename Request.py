0d8330382a010311a55387e4ec5aebedc8a39e75
ofir <ofir.birka@mail.huji.ac.il>
2017-11-27 10:24:09 +0200

    Request Module

--- Request.py
+++ Request.py
@@ -1,12 +1,27 @@
 import os
 
-# TODO documentation (@Ofir [look at Response and do something equivalent]) 
+"""
+    This class is an abstract interface wrapper for a request to any module of webSight.
+    You must use this class as a request to specific module you want to use.
+"""
 class Request:
+
+    """
+    Request c-tor
+
+    Args:
+        - imgPath: a path to an image you want to load to webSite
+
+    Attributes:
+        - (private) _imgPath: a string that contains the image path
+    """
     def __init__(self, imgPath):
         if(os.path.exists(imgPath)):
             self._imgPath = imgPath
 
-
+    """
+    Returns the image
+    """
     def getImage(self):
         if(self._imgPath != ''):
             return None
